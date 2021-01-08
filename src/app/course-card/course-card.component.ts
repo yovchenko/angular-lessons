feb4ae66838f8c54f263cc4dfbdaebf03004389f
yovchenko <volodymyr.yovchenko@gmail.com>
2021-01-08 20:00:18 +0200

    feat(style): add css styles to created components

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -11,6 +11,9 @@ export class CourseCardComponent implements OnInit {
   @Input()
   course:Course;
 
+  @Input()
+  cardIndex: number;
+
   @Output('courseSelected')
   courseEmmiter = new EventEmitter<Course>();
 
