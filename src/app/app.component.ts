96a985166513449c244eea4ab042c93c0d19787c
yovchenko <volodymyr.yovchenko@gmail.com>
2021-01-09 15:25:49 +0200

    feat(app): add pipes to the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,16 @@ export class AppComponent {
 
   courses = COURSES;
 
+  startDate: Date = new Date(2020, 0, 1)
+
+  title = COURSES[1].description;
+
+  price = COURSES[1].price;
+
+  rate = COURSES[1].rate;
+
+  course = COURSES[1];
+  
   onCourseSelected(course: Course) {
     console.info("App component - custom event " + course.description);
   }
